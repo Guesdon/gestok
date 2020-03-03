64b9681be52013df1ea982c66b4cb2bc3613da13
GuesdonJ <julien.gues@gmail.com>
2020-03-03 16:00:27 +0100

    11-2

--- readmeProcess/index.php
+++ readmeProcess/index.php
@@ -1,3 +1,10 @@
 <?php
-    header ('Location: /index.php');
-    exit();
+    //header ('Location: /index.php');
+    //exit();
+
+    // echo "<pre>";
+    // var_dump($_SERVER);
+    // echo "</pre>";
+    // echo "index dans readmeProcess";
+
+    echo  $_SERVER['PHP_SELF']."<br>";
